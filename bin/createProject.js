15277940f43be98117d473930eb1e8c69794da46
arya2004 <arya20j4@gmail.com>
2024-06-17 16:02:43 +0530

    refactor: organized code

---
+++ bin/createProject.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+import { getProjectName, selectDb, selectStructure, selectRenderEngine } from '../interactive.js';
+import { createProject } from '../lib/setup.js';
+
+// Extract command line arguments
+const args = process.argv.slice(2); // Exclude 'node' and script name
+
+(async () => {
+  if (args[0] === 'init') {
+    const projectName = await getProjectName();
+    const structure = await selectStructure();
+    const database = await selectDb();
+    const renderEngine = await selectRenderEngine();
+    createProject(projectName, structure, database, renderEngine);
+  }
+  if (args[0] === 'new') {
+    const folderName = args[1];
+    createProject(folderName, 'API', 'MongoDb');
+  }
+})();
--- interactive.js
+++ interactive.js
@@ -1,106 +1,52 @@
 import inquirer from 'inquirer';
 
-
 export const getProjectName = async () => {
-    const answer = await inquirer.prompt([
-      {
-        type: 'input',
-        name: 'userName',
-        message: 'Enter project name:'
-      }
-    ]);
-    console.log(`Project name ${answer.userName}!`);
-    return answer.userName;
-  };
-    
+  const answer = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'userName',
+      message: 'Enter project name:'
+    }
+  ]);
+  console.log(`Project name ${answer.userName}!`);
+  return answer.userName;
+};
 
 export const selectStructure = async () => {
-    const answer = await inquirer.prompt([
-      {
-        type: 'list',
-        name: 'selectedColor',
-        message: 'Select Structure:',
-        choices: ['MVC', 'API']
-      }
-    ]);
-    console.log(`Structure is ${answer.selectedColor}.`);
-    return answer.selectedColor;
-  };
-
-  export const selectRenderEngine = async () => {
-    const answer = await inquirer.prompt([
-      {
-        type: 'list',
-        name: 'selectedColor',
-        message: 'Select Structure:',
-        choices: ['EJS', 'PUG', 'Handlebars']
-      }
-    ]);
-    console.log(`Structure is ${answer.selectedColor}.`);
-    return answer.selectedColor;
-  };
-
-export const  selectDb = async () => {
-    const answer = await inquirer.prompt([
-      {
-        type: 'list',
-        name: 'selectedColor',
-        message: 'Select Db integration:',
-        choices: ['MongoDb', 'Postgre', 'MySQL', 'SQLServer']
-      }
-    ]);
-    console.log(`Db is ${answer.selectedColor}.`);
-    return answer.selectedColor;
-  };
-
-/////////////////////////////////////////////////////////////////
-
-const options = [
-  'Option 1',
-  'Option 2',
-  'Option 3'
-];
-
-export const selectOption = async () => {
   const answer = await inquirer.prompt([
     {
       type: 'list',
-      name: 'selectedOption',
-      message: 'Select an option:',
-      choices: options
+      name: 'selectedStructure',
+      message: 'Select Structure:',
+      choices: ['MVC', 'API']
     }
   ]);
-  console.log(`You selected: ${answer.selectedOption}`);
+  console.log(`Structure is ${answer.selectedStructure}.`);
+  return answer.selectedStructure;
 };
 
-export const askConfirmation = async () => {
-    const answer = await inquirer.prompt([
-      {
-        type: 'confirm',
-        name: 'confirmation',
-        message: 'Are you sure you want to proceed?'
-      }
-    ]);
-    if (answer.confirmation) {
-      console.log('Proceeding...');
-    } else {
-      console.log('Cancelled.');
+export const selectRenderEngine = async () => {
+  const answer = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'selectedRenderEngine',
+      message: 'Select Render Engine:',
+      choices: ['EJS', 'PUG', 'Handlebars']
     }
-  };
+  ]);
+  console.log(`Render Engine is ${answer.selectedRenderEngine}.`);
+  return answer.selectedRenderEngine;
+};
 
-
-
- export const selectFruits = async () => {
-    const answer = await inquirer.prompt([
-      {
-        type: 'checkbox',
-        name: 'selectedFruits',
-        message: 'Select your favorite fruits:',
-        choices: ['MongoDb', 'Postgre', 'MySQL', 'SQLServer']
-      }
-    ]);
-    console.log('Your selected fruits:');
-    answer.selectedFruits.forEach(fruit => console.log(fruit));
-  };
-  
-
+export const selectDb = async () => {
+  const answer = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'selectedDb',
+      message: 'Select Db integration:',
+      choices: ['MongoDb', 'Postgre', 'MySQL', 'SQLServer']
+    }
+  ]);
+  console.log(`Db is ${answer.selectedDb}.`);
+  return answer.selectedDb;
+};

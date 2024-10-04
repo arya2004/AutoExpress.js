e8e36e78b9cf5e27f6f0f6744a0faf0cdeb2ddd6
Aaditya8C <aadityajp0419@gmail.com>
2024-10-04 23:42:59 +0530

    flat_option_added

--- bin/createProject.js
+++ bin/createProject.js
@@ -1,21 +1,27 @@
 #!/usr/bin/env node
 
-import { getProjectName, selectDb, selectStructure, selectRenderEngine } from '../interactive.js';
-import { createProject } from '../lib/setup.js';
+import {
+  getProjectName,
+  selectDb,
+  selectStructure,
+  selectRenderEngine,
+} from "../interactive.js";
+import { createProject } from "../lib/setup.js";
 
 // Extract command line arguments
 const args = process.argv.slice(2); // Exclude 'node' and script name
 
 (async () => {
-  if (args[0] === 'init') {
+  const isFlat = args.includes("--flat");
+  if (args[0] === "init") {
     const projectName = await getProjectName();
     const structure = await selectStructure();
     const database = await selectDb();
     const renderEngine = await selectRenderEngine();
-    createProject(projectName, structure, database, renderEngine);
+    createProject(projectName, structure, database, renderEngine, isFlat);
   }
-  if (args[0] === 'new') {
+  if (args[0] === "new") {
     const folderName = args[1];
-    createProject(folderName, 'API', 'MongoDb');
+    createProject(folderName, "API", "MongoDb", isFlat);
   }
 })();
